039df08392d3ecfb4be74d41f0f284ecc6746054
y16ra <y16ra@users.noreply.github.com>
2022-03-21 16:16:37 +0900

    update main.go

--- go.mod
+++ go.mod
@@ -3,8 +3,6 @@ module github.com/y16ra/crypto-currency
 go 1.15
 
 require (
-	github.com/kr/pretty v0.1.0 // indirect
-	github.com/stretchr/testify v1.4.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	github.com/y16ra/gmo-coin-go-sdk/client v0.0.0-20220321064831-97cf169a716b
 	gopkg.in/ini.v1 v1.66.4
 )
--- main.go
+++ main.go
@@ -1,27 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 
-	"github.com/y16ra/gmo-coin-go-sdk/client"
+	client "github.com/y16ra/gmo-coin-go-sdk/client"
 )
 
 func main() {
 	// GMO Coin client
-	gmoClient := new(client.GmoCoin).NewClient()
-
-	// ticker := gmoClient.Ticker.All()
-	// fmt.Println(ticker)
-	// assetsResp := gmoClient.Account.Assets()
-	// fmt.Printf("%+v \n", assetsResp)
-
-	// merginResp := gmoClient.Account.Margin()
-	// fmt.Printf("%+v \n", merginResp)
-
-	// orderParam := "orderId=1145804966"
-	// orderResp := gmoClient.Order.OrderList(orderParam)
-	// fmt.Printf("%+v \n", orderResp)
-
-	aOrders := gmoClient.Order.ActiveOrderList("BTC")
-	fmt.Printf("%+v \n", aOrders)
+	cfg := client.NewConfiguration()
+	apiClient := client.NewAPIClient(cfg)
+	ctx := context.Background()
+	req := apiClient.PublicApi.PublicV1StatusGet(ctx)
+	res, _, _ := req.Execute()
+	fmt.Println(res.Data)
 }
